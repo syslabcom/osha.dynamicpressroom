5dcd1a65d9912ea3b2db88dc182fb44253bdd621
brand <brand@97d050b9-b799-4550-a901-4f04598d4601>
2009-10-08 17:00:32 +0000

    Add description

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ version = '0.1'
 
 setup(name='osha.dynamicpressroom',
       version=version,
-      description="",
+      description="Turns PressRoom objects into Dynamic Pressrooms!",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
